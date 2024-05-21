b654b90c6455456a7561b0322ed6366f57299dd7
SaqeebPatel <saqeeb3p@gmail.com>
2024-05-21 23:07:39 +0530

    add the ass 2

---
+++ Assignment 2/palindrome.js
@@ -0,0 +1,14 @@
+function isPalindrome(str){
+    let rev="";
+    for(let i=str.length-1;i>=0;i--){
+        rev+=str[i];
+    }
+    if(rev==str){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+console.log(isPalindrome("mukta"));
+console.log(isPalindrome("madam"));
