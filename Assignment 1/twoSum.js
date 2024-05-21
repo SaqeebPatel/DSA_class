d893baa9c917e6591abb589c3ca52a4dc25528cd
SaqeebPatel <saqeeb3p@gmail.com>
2024-05-21 20:59:53 +0530

    task is updated

---
+++ Assignment 1/twoSum.js
@@ -0,0 +1,17 @@
+var twoSum = function(nums, target) {
+    for (var i = 0; i < nums.length; i++) {
+        for (var j = i + 1; j < nums.length; j++) {
+            var sum = nums[i] + nums[j];
+            if (sum === target) {
+                return [i, j];
+            }
+        }
+    }
+    return []; 
+};
+
+
+var nums = [2, 7, 11, 15];
+var target = 9;
+var result = twoSum(nums, target);
+console.log(result);
